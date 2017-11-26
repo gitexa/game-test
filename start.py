e1f1c00d7534676821777abd524def17e87e3918
gitexa <uafla@student.kit.edu>
2017-11-26 22:18:59 +0100

    Old changes

--- start.py
+++ start.py
@@ -1,4 +1,16 @@
 import keyboard
+import os
+
+
+# Open application
+print("Start libreoffice")
+os.system('libreoffice --writer')
+
+time.sleep(3)    # pause 5.5 seconds
+
+print("Write text")
+
+
 
 # Manipulate keyboard and mouse with PiPy-Package https://github.com/boppreh/keyboard
-keyboard.write('The quick brown fox jumps over the lazy dog.')
+#keyboard.write(' The quick brown fox jumps over the lazy dog.')
