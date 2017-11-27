bf29c3af6238c743e7ed4e98c116898cbc4fa610
gitexa <uafla@student.kit.edu>
2017-11-27 03:35:48 +0100

    YOLO and OCR working on frames and cropped parts

--- ocr_timer.py
+++ ocr_timer.py
@@ -7,15 +7,15 @@ import os
 import config as cfg
 
 image = cv2.imread('frame_crop.jpg')
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+#gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 #gray = cv2.medianBlur(gray, 3)
 
-gray = cv2.threshold(gray, 0, 255,
-		cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+#gray = cv2.threshold(gray, 0, 255,
+#		cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
 
 filename = "{}.png".format(os.getpid())
-cv2.imwrite(filename, gray)
+cv2.imwrite(filename, image)
 
 text = pytesseract.image_to_string(Image.open(filename))
 #os.remove(filename)
--- parseVideoFile.py
+++ parseVideoFile.py
@@ -3,8 +3,8 @@ import config as cfg
 
 ff = ffmpy.FFmpeg(
     inputs={'cs_test.mp4':None },
-    outputs={'frame.jpg': ' -ss 00:00:14 -t 00:00:2 -s 640x360 -r 1 -f mjpeg',
-    'frame_crop.jpg': '-filter:v "crop=50:30:322:333" -ss 00:00:14 -t 00:00:2 -s 640x360 -r 1 -f mjpeg'}
+    outputs={'frame.jpg': ' -ss 00:00:16 -t 00:00:2 -s 640x360 -r 1 -f mjpeg',
+    'frame_crop.jpg': '-filter:v "crop=50:30:322:333" -ss 00:00:16 -t 00:00:2 -s 640x360 -r 1 -f mjpeg'}
     )
 
 ff.run()
